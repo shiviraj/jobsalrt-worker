415258d05c6dd6168ac33f62a3ca4b6278cc3c7e
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-06-05 20:52:19 +0530

    Added worker to update the jobs and removed unnecessary tasks

--- src/main/kotlin/com/jobsalrt/worker/service/postService/PostService.kt
+++ src/main/kotlin/com/jobsalrt/worker/service/postService/PostService.kt
@@ -1,9 +1,7 @@
 package com.jobsalrt.worker.service.postService
 
-import com.jobsalrt.worker.controller.view.FilterRequest
 import com.jobsalrt.worker.domain.Post
 import com.jobsalrt.worker.repository.PostRepository
-import com.jobsalrt.worker.repository.PostRepositoryOps
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
@@ -12,7 +10,6 @@ import java.time.LocalDateTime
 @Service
 class PostService(
     @Autowired private val postRepository: PostRepository,
-    @Autowired private val postRepositoryOps: PostRepositoryOps,
 ) {
 
     fun save(post: Post): Mono<Post> {
@@ -23,20 +20,12 @@ class PostService(
         return postRepository.findBySource(source)
     }
 
-    fun getAllPosts(page: Int, filterRequest: FilterRequest): Mono<List<Post>> {
-        return postRepositoryOps.findPosts(filterRequest, page).collectList()
-    }
-
-    fun getPostsPageCount(filterRequest: FilterRequest): Mono<Pair<Long, Double>> {
-        return postRepositoryOps.findPostCount(filterRequest)
-    }
-
     fun getPostByUrl(url: String): Mono<Post> {
-        return postRepositoryOps.findByBasicDetailsUrl(url)
+        return postRepository.findByBasicDetailsUrl(url)
     }
 
     fun updatePost(url: String, post: Post): Mono<Post> {
-        return postRepositoryOps.findByBasicDetailsUrl(url)
+        return postRepository.findByBasicDetailsUrl(url)
             .flatMap {
                 post.id = it.id
                 post.postUpdateDate = LocalDateTime.now()
@@ -45,7 +34,7 @@ class PostService(
     }
 
     fun urlAvailable(url: String): Mono<Pair<Boolean, String>> {
-        return postRepositoryOps.findByBasicDetailsUrl(url)
+        return postRepository.findByBasicDetailsUrl(url)
             .map {
                 Pair(false, it.source)
             }
