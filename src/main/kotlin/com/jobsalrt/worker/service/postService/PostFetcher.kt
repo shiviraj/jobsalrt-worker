c1f7ad94de41b9d44c436e1687d8090f624cc7e5
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-06-20 07:13:09 +0530

    removed the link from a tag which is not starts from http while saving raw post

--- src/main/kotlin/com/jobsalrt/worker/service/postService/PostFetcher.kt
+++ src/main/kotlin/com/jobsalrt/worker/service/postService/PostFetcher.kt
@@ -52,10 +52,7 @@ abstract class PostFetcher(
 
     @Transactional(rollbackForClassName = ["Exception"])
     fun updateRawPostIfAvailable(document: Document, jobUrl: JobUrl): Mono<RawPost> {
-        val html = parseHtml(document)
-            .replace(Regex("<[^a][^>]*>", RegexOption.IGNORE_CASE), "")
-            .replace(Regex("<a.*href=\"([^h])[^>]*>", RegexOption.IGNORE_CASE), "")
-            .replace(Regex("[ \n\t]"), "")
+        val html = parseHTMLText(document)
 
         return rawPostService.findBySource(jobUrl.url)
             .flatMap { rawPost ->
@@ -77,7 +74,7 @@ abstract class PostFetcher(
                 val post = createPost(Post(source = jobUrl.url), document)
                 Mono.zip(
                     postService.save(post),
-                    rawPostService.save(RawPost(html = parseHtml(document), source = jobUrl.url))
+                    rawPostService.save(RawPost(html = parseHTMLText(document), source = jobUrl.url))
                 )
                     .onErrorContinue { throwable, u ->
                         println(u)
@@ -89,6 +86,13 @@ abstract class PostFetcher(
             .map {
                 it.t2
             }
+    }
+
+    private fun parseHTMLText(document: Document): String {
+        return parseHtml(document)
+            .replace(Regex("<[^a][^>]*>", RegexOption.IGNORE_CASE), "")
+            .replace(Regex("<a.*href=\"([^h])[^>]*>", RegexOption.IGNORE_CASE), "")
+            .replace(Regex("[ \n\t]"), "")
     }
 
     private fun fetchPostFromUrl(url: String): Mono<Document> {
